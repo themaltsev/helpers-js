f8e8a10fb6fd7e549959babc01dc68dfc2724cd4
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2020-09-15 04:34:27 +0300

    Create input-mask.js

---
+++ input-mask.js
@@ -0,0 +1,72 @@
+function setCursorPosition(pos, elem) {
+
+    elem.focus();
+
+    if (elem.setSelectionRange) elem.setSelectionRange(pos, pos);
+
+    else if (elem.createTextRange) {
+
+        let range = elem.createTextRange();
+
+        range.collapse(true);
+
+        range.moveEnd("character", pos);
+
+        range.moveStart("character", pos);
+
+        range.select()
+
+    }
+
+}
+
+
+function mask(event) {
+
+    // let matrix = "+7 (___) ___ ____",
+    let matrix = " +7 (___)-___-__-__",
+
+        i = 0,
+
+        def = matrix.replace(/\D/g, ""),
+
+        val = this.value.replace(/\D/g, "");
+
+    if (def.length >= val.length) val = def;
+
+    this.value = matrix.replace(/./g, function(a) {
+
+        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+
+    });
+
+    if (event.type == "blur") {
+
+        if (this.value.length == 2) this.value = ""
+
+    } else setCursorPosition(this.value.length, this)
+
+};
+
+
+export function inputMask() {
+
+const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+
+        let input = entry.target;
+
+        input.addEventListener("input", mask, false);
+
+        input.addEventListener("focus", mask, false);
+
+        input.addEventListener("blur", mask, false);
+
+        }
+    });
+    });
+
+$$('.num_check').forEach(e=>imageObserver.observe(e))
+
+}
