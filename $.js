de20b9c4d871c0030de20289eb7c9024c518ba10
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2020-09-15 03:31:15 +0300

    Update $.js

--- $.js
+++ $.js
@@ -6,13 +6,9 @@ window.malert = new madAlert();
 import { gsap } from "@l/gsap";
 window.tl = gsap.timeline();
 window.axios = require("axios");
-import { CustomPush } from "@p/custom-push";
-window.CustomPush = new CustomPush();
-import { zHidden } from "@z/zayavka-hidden";
-window.zHidden = new zHidden();
 
 window.soundPush = (url) => {
-  if (window.innerWidth > 768) {
+  if (window.innerWidth > 768) {//если ширина окна больше 768
     let audio = new Audio(); // Создаём новый элемент Audio
     audio.src = url; // Указываем путь к звуку "клика"
     audio.autoplay = true; // Автоматически запускаем
@@ -20,19 +16,3 @@ window.soundPush = (url) => {
 };
 
 
-window.cutText = (el,count) =>{
-  $$(el).forEach(e => {
-     let fullText = e.textContent
-     if(fullText.length > count) {
-      let cutText = `${e.textContent.slice(0,count)}...`
-      e.textContent = cutText
-      let readMore = document.createElement('div');
-      readMore.classList.add('read__more')
-      readMore.textContent = `Читать ещё`
-      e.appendChild(readMore)
-      e.onclick = () => {
-       e.innerText = fullText
-      }
-     }
-  });
-}
