9c591f23ace6e24072d441479836f11e25e5e413
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2021-01-07 22:46:34 +0300

    Update timeNow.js

--- timeNow.js
+++ timeNow.js
@@ -1,1 +1,3 @@
-let timeNow = new Date().toLocaleTimeString()
+let timeNow = new Date().toLocaleTimeString() //Время
+let DataNow = new Date().toLocaleDateString() //Дата
+let allData = new Date().toLocaleString() //Дата и Время
