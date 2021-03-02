d965bb2e619cc8e3a1d0394728c96c7636dbfadd
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2021-03-03 01:58:36 +0300

    Update sortArrayToKey.js

--- sortArrayToKey.js
+++ sortArrayToKey.js
@@ -1,3 +1,6 @@
   let dataRelivant = dataSearch.sort((a, b) => a.views === b.views ? 0 : a.views > b.views || -1).reverse()
 
   console.log(dataRelivant);
+
+
+dataSearch.sort((a,b) => b.views - a.views) 
