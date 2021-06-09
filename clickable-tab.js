5e06ad2d304e095f38b4e307247035e0bb0f166c
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2021-06-10 01:03:09 +0300

    Create clickable-tab.js

---
+++ clickable-tab.js
@@ -0,0 +1,15 @@
+  // function lazyTapIcon() {
+  //   let item = new IntersectionObserver((entries, observer) => {
+  //     entries.forEach((entry) => {
+          
+  //       if (entry.isIntersecting) {
+
+  //         entry.target.innerHTML += `<img class="__clickable__item"  src="/img/icons/tap.svg" loading="lazy" alt="Сликабельный элемент">`
+
+  //       }
+  //     });
+  //   });
+  //   $$(".__clickable").forEach((e) => item.observe(e));
+  // }
+
+  // lazyTapIcon()
