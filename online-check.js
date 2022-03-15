d1fb01ce1754bc1a921d4103484da94c06a99b03
Aleksandr Maltsev <47543995+themaltsev@users.noreply.github.com>
2022-03-15 21:33:20 +0300

    Update online-check.js

--- online-check.js
+++ online-check.js
@@ -1,5 +1,25 @@
-if(navigator.onLine){
-  alert('online');
- } else {
-  alert('offline');
- }
+let statOffline = false
+
+let offlineTimeOut = 25
+
+window.addEventListener("offline", (event) => {
+cPush.show({
+title:`Соединение потеряно с интернетом.`
+})
+statOffline = true
+
+setTimeout(() => {
+
+  if(statOffline) window.location.href = '/offline'
+
+}, (offlineTimeOut - 2) * 1000);
+
+});
+
+
+window.addEventListener("online", (event) => {
+cPush.show({
+title:`Соединение с интернетом восстановлено.`
+})
+statOffline = false
+});
